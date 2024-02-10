77394a40521146290fb8755beb24e3cc7cfea539
llcain <llcain2012@gmail.com>
2024-02-10 15:03:16 -0600

    Updated controllers

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,39 +1,29 @@
 // require express
 const express = require('express')
-const User = require('../models/user')
+// const User = require('../models/user')
+const {
+    createUser,
+    getUser,
+    getUsers,
+    deleteUser,
+    updateUser
+} = require('../controllers//user')
 
 const router = express.Router()
 
 // Get all users
-router.get('/', (req, res) => {
-    res.json({mssg: "Get all users"})
-})
+router.get('/', getUsers)
 
 // Get a single user
-router.get('/:id', (req,res) => {
-    res.json({mssg: "Get a single user"})
-})
+router.get('/:id', getUser)
 
 // Post a new user
-router.post('/', async (req, res) => {
-    const {name, age} = req.body
-    try {
-        const user = await User.create({name, age})
-        res.status(200).json(user)
-
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }    
-})
+router.post('/', createUser)
 
 // Delete a user
-router.delete('/:id', (req, res) => {
-    res.json({mssg: "delete a user"})
-})
+router.delete('/:id', deleteUser)
 
 // Update a user
-router.patch('/:id', (req, res) => {
-    res.json({mssg: "Update a user"})
-})
+router.patch('/:id', updateUser)
 
 module.exports = router
